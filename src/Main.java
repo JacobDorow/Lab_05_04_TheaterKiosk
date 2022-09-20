884334fb397593724eafe9aa6353726c23d9fbc2
JacobDorow <dorowjm@mail.uc.edu>
2022-09-20 13:07:46 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int personAge = 21;
+        System.out.println("(Simulated Input) Welcome to the theatre. Please enter your age: " + personAge);
+        if (personAge >= 21) {
+            System.out.println("You get a paper wrist band");
+        }
+    }
+}
